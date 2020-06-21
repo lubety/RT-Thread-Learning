5c741e7a58cb79239e543bc1bde1291b338a971b
lubety <lubety@126.com>
2020-06-21 12:18:53 +0800

    增加13章，移植工程后上传

---
+++ rt-thread-nano/USER/board.h
@@ -0,0 +1,17 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+
+#include "stm32f10x.h"
+#include "led.h"
+
+
+void rt_hw_board_init(void);
+void SysTick_Handler(void);
+
+
+
+
+
+
+#endif
---
+++ rt-thread-nano/USER/main.c
@@ -0,0 +1,17 @@
+//#include "sys.h"
+//#include "delay.h"
+//#include "usart.h"
+//#include "led.h"
+// 
+
+#include "board.h"
+#include "rtthread.h"
+
+ int main(void)
+ {	  
+	return 0;
+ }
+
+
+
+
