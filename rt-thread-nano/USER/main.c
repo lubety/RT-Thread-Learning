ce6d35569a48c774e61b41ea4660241b0d828433
lubety <lubety@126.com>
2020-07-07 23:17:41 +0800

    增加消息队列实验代码，并且进行测试

--- rt-thread-nano/USER/main.c
+++ rt-thread-nano/USER/main.c
@@ -18,6 +18,14 @@ static void led2_thread_entry(void* parameter);
 static rt_thread_t key_thread = RT_NULL;
 static void key_thread_entry(void* parameter);
 
+//消息队列数据结构创建
+static rt_mq_t test_mq = RT_NULL;
+static rt_thread_t receive_thread = RT_NULL;
+static rt_thread_t send_thread = RT_NULL;
+static void receive_thread_entry(void *parameter);
+static void send_thread_entry(void *parameter);
+
+
 int main(void)
 {
 	rt_thread_init(&led1_thread,
@@ -28,7 +36,7 @@ int main(void)
 					sizeof(rt_led1_thread_stack),
 					3,
 					20);
-					
+				
 	led2_thread = 
 	rt_thread_create("led2",
 					led2_thread_entry,
@@ -42,7 +50,7 @@ int main(void)
 		rt_thread_startup(led2_thread);
 	else
 		return -1;
-	
+#if 0		
 	key_thread = 
 	rt_thread_create("key",
 					key_thread_entry,
@@ -53,7 +61,41 @@ int main(void)
 	if(key_thread != RT_NULL)
 		rt_thread_startup(key_thread);
 	else
-		return -1;				
+		return -1;
+#endif
+	
+	
+	//消息队列实验
+	test_mq = rt_mq_create("test_mq",
+							40,
+							20,
+							RT_IPC_FLAG_FIFO);
+	if(test_mq != RT_NULL)
+		rt_kprintf("消息队列创建成功！\n\n");
+	
+	
+	receive_thread = rt_thread_create("receive",
+							receive_thread_entry,
+							RT_NULL,
+							512,
+							3,
+							20);
+	if(receive_thread != RT_NULL)
+		rt_thread_startup(receive_thread);
+	else
+		return -1;
+	
+	send_thread = rt_thread_create("send",
+							send_thread_entry,
+							RT_NULL,
+							512,
+							2,
+							20);
+	if(send_thread != RT_NULL)
+		rt_thread_startup(send_thread);
+	else
+		return -1;
+	
 					
 	return 0;
 }
@@ -87,8 +129,6 @@ u8 KEY_Scan(void)
 	static unsigned short keyFilterTimer[KEY_NUM] = {0};
 	static u32 keyValue;
 	
-	rt_thread_delay(10);
-	
 	tempKeyVal = KEY_Read();
 	tempMask = 0;
 	for(i = 0; i < KEY_NUM; i++)
@@ -119,6 +159,7 @@ static void key_thread_entry(void* parameter)
 	
 	while(1)
 	{
+		rt_thread_delay(10);
 		key=KEY_Scan();	//得到键值
 		if(keyBak != key)
 		{
@@ -167,4 +208,62 @@ static void key_thread_entry(void* parameter)
 }	
 
 
+
+
+static void receive_thread_entry(void *parameter)
+{
+	rt_err_t uwRet = RT_EOK;
+	uint32_t r_queue;
+	
+	while(1)
+	{
+		uwRet = rt_mq_recv(test_mq, &r_queue, sizeof(r_queue), RT_WAITING_FOREVER);
+		if(uwRet == RT_EOK)
+		{
+			rt_kprintf("本次接收到的数据是：%d\n", r_queue);
+		}
+		else
+		{
+			rt_kprintf("数据接收出错，错误代码：0x%lx\n", uwRet);
+		}
+		rt_thread_delay(200);
+	}
+}
+
+static void send_thread_entry(void *parameter)
+{
+	rt_err_t uwRet = RT_EOK;
+	uint32_t send_data1 = 1;
+	uint32_t send_data2 = 2;
+	uint32_t key = 0;
+	static uint32_t keyBak = 0;
+	
+	while(1)
+	{
+		rt_thread_delay(10);
+		
+		key=KEY_Scan();	//得到键值
+		if(keyBak != key)
+		{
+			keyBak = key;			
+			switch(key)
+			{				 
+				case WKUP_PRES:
+				case KEY2_PRES:	
+					uwRet = rt_mq_send(test_mq, &send_data1, sizeof(send_data1));
+					if(uwRet != RT_EOK)
+						rt_kprintf("数据不能发送到消息队列！错误代码：%lx\n", uwRet);
+					break;
+				case KEY1_PRES:
+				case KEY0_PRES:
+					uwRet = rt_mq_send(test_mq, &send_data2, sizeof(send_data2));
+					if(uwRet != RT_EOK)
+						rt_kprintf("数据不能发送到消息队列！错误代码：%lx\n", uwRet);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
 
